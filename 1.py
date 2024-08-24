cf338c0c66dd008b8f72cbf7d0256f42841b8d78
Benjamin Vallory <serversadmin@bitskingdom.com>
2024-08-24 17:23:43 -0300

    cambios

--- 1.py
+++ 1.py
@@ -1,4 +1,32 @@
- #joako
+# Fede punto 1-B
+def es_abc(palabra):
+    palabra = palabra.lower()
+    return list(palabra) == sorted(palabra)
+
+
+palabra = input("Ingrese una palabra: ")
+
+print(es_abc(palabra))
+
+
+
+#Mateo punto 1-C
+"""Escriba un procedimiento procesar_palabras(entrada) que acepte una
+secuencia de palabras separadas por coma, las ordene y las imprima.
+Suponiendo que la entrada provista al programa es la siguiente:
+te,felicito,que,bien,actuas
+La salida esperada es: actuas,bien,felicito,que,te"""
+
+def proocesar_palabras(entrada):
+    lista= entrada.split(',')
+    ordenadas= sorted(lista)
+    salida= ','.join(ordenadas)
+    print(f"{salida}\n")
+    
+entrada_usuario= input("\nIngrese una secuencia de palabras separadas por coma: ")
+proocesar_palabras(entrada_usuario)
+
+
 # Benja punto 1-D)
 def listas_diferencia(lista1, lista2):
     set1 = set(lista1)
@@ -32,34 +60,6 @@ def numeros_par_impar(entrada):
 entrada_usuario = input("coloca una lista de números separados por coma: ")
 numeros_par_impar(entrada_usuario)
 
-
-# Fede punto 1-B
-def es_abc(palabra):
-    palabra = palabra.lower()
-    return list(palabra) == sorted(palabra)
-
-
-palabra = input("Ingrese una palabra: ")
-
-print(es_abc(palabra))
-
-
-
-#Mateo punto 1-C
-"""Escriba un procedimiento procesar_palabras(entrada) que acepte una
-secuencia de palabras separadas por coma, las ordene y las imprima.
-Suponiendo que la entrada provista al programa es la siguiente:
-te,felicito,que,bien,actuas
-La salida esperada es: actuas,bien,felicito,que,te"""
-
-def proocesar_palabras(entrada):
-    lista= entrada.split(',')
-    ordenadas= sorted(lista)
-    salida= ','.join(ordenadas)
-    print(f"{salida}\n")
-    
-entrada_usuario= input("\nIngrese una secuencia de palabras separadas por coma: ")
-proocesar_palabras(entrada_usuario)
 
 # Benja punto 1-F)
 import re
